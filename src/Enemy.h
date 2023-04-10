e6fa1f26756a5d420a0563cc6573f8b132dc703f
Che Liu <liuche@connect.hku.hk>
2023-04-10 13:35:37 +0800

    remove redundant code

--- src/Enemy.h
+++ src/Enemy.h
@@ -14,13 +14,3 @@ class Enemy {
         int LX, LY, RX, RY, x, y;
         char sym;
 };
-
-class Enemies {
-    public:
-        Enemies(int _LX, int _LY, int _RX, int _RY);
-        void draw(WINDOW* win);
-        void add_enemy(int x, int y, char sym);
-    private:
-        int LX, LY, RX, RY;
-        vector<Enemy> enemies;
-};
